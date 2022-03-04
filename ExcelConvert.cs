9b93ae0ac5e31642c10a71302e25ed0ee85632d8
lcz <1402568761@qq.com>
2022-03-04 11:09:58 +0800

    commit

---
+++ ExcelConvert.cs
@@ -0,0 +1,156 @@
+﻿using ExcelDataReader;
+using ExcelNumberFormat;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace E2C
+{
+    class ExcelConvert
+    {
+        public string mOutPath = "";
+
+        public const int mDoublePointKeepCnt = 5;
+
+        public ExcelConvert(string outPath)
+        {
+            mOutPath = outPath;
+        }
+
+        public int getDoublePointCnt(String param)
+        {
+            String[] ss = param.Split(".");
+
+            if (ss.Length <= 1)
+            {
+                return 0;
+            }
+
+            return ss[1].Length;
+        }
+
+        public string GetFormattedValue(IExcelDataReader reader, int columnIndex, CultureInfo culture)
+        {
+            var value = reader.GetValue(columnIndex);
+
+            var formatString = reader.GetNumberFormatString(columnIndex);
+            if (formatString != null)
+            {
+                var format = new NumberFormat(formatString);
+
+                var forstr = format.Format(value, culture);
+
+                if (value != null && value.GetType() == typeof(System.Double) && getDoublePointCnt(forstr) > mDoublePointKeepCnt)
+                {
+                    forstr = Convert.ToDouble(forstr).ToString("F" + mDoublePointKeepCnt.ToString());
+                    forstr = Convert.ToDouble(forstr).ToString("G");
+                }
+
+                return forstr;
+            }
+            return Convert.ToString(value, culture);
+        }
+
+        public void ConvertCsv(string file)
+        {
+            CultureInfo culture = CultureInfo.GetCultureInfo("zh-cn");
+
+            if (file.EndsWith(".xlsx"))
+            {
+                try
+                {
+                    var stream = File.Open(file, FileMode.Open, FileAccess.Read);
+                    var reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    do
+                    {
+
+                        ConverToCSV(reader, culture);
+
+                    } while (reader.NextResult());
+                }
+                catch
+                {
+                    Console.WriteLine("警告：文件已被其他线程处理 " + file + ",请截图给维护人员 ");
+                }
+            }
+        }
+
+
+        private void DealEmoji(ref string str)
+        {
+            str = Regex.Replace(str, @"\p{Cs}", "");
+            str = Regex.Replace(str, @"\n", System.Environment.NewLine);
+            str = str.Replace("\"", "\"\"");
+            str = str.Trim();
+        }
+
+        private bool ConverToCSV(IExcelDataReader reader, CultureInfo culture)
+        {
+            // sheets in excel file becomes tables in dataset
+            // result.Tables[0].TableName.ToString(); // to get sheet name (table name)
+
+            var csvCon = new System.Text.StringBuilder("");
+
+            while (reader.Read())
+            {
+                bool isNullRow = true;
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    var str = GetFormattedValue(reader, i, culture);
+
+                    if (str != "")
+                    {
+                        isNullRow = false;
+                        break;
+                    }
+                }
+
+                if (isNullRow)
+                {
+                    continue;
+                }
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    var str = GetFormattedValue(reader, i, culture);
+
+                    DealEmoji(ref str);
+
+                    if (str.Contains(System.Environment.NewLine) || str.Contains(","))
+                    {
+                        str = "\"" + str + "\"";
+                    }
+
+                    if (i < reader.FieldCount - 1)
+                    {
+                        csvCon.Append(str + ",");
+                    }
+                    else
+                    {
+                        csvCon.Append(str + System.Environment.NewLine);
+                    }
+                }
+            }
+
+            try
+            {
+                string output = mOutPath + Path.DirectorySeparatorChar + reader.Name + ".csv";
+                StreamWriter csv = new StreamWriter(@output, false, Encoding.UTF8);
+                csv.Write(csvCon.ToString());
+                csv.Close();
+            }
+            catch
+            {
+                Console.WriteLine("INFO：跳过重复的表 " + reader.Name);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace E2C
+{
+    class Program
+    {
+        static int fileIdx = 0;
+        static string xlsPath = @"xls_tmp";
+        static string csvPath = @"csv";
+
+        static void DoCovert(object fileName)
+        {
+            fileIdx++;
+
+            Console.WriteLine("正在处理第" + fileIdx + "个文件 :" + fileName.ToString());
+
+            ExcelConvert ct = new ExcelConvert(csvPath);
+
+            ct.ConvertCsv(fileName.ToString());
+        }
+
+        static int ParseArgs(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Process processes = Process.GetCurrentProcess();
+
+                Console.WriteLine(@"参数错误，用法：" + processes.ProcessName + " " + xlsPath + Path.DirectorySeparatorChar + " " + csvPath);
+                return -1;
+            }
+
+            xlsPath = args[0];
+            csvPath = args[1];
+
+            Console.WriteLine(@"参数设置成功 " + xlsPath + " " + csvPath);
+
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int iRet = ParseArgs(args);
+            if (iRet != 0)
+			{
+
+                return;
+			}
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);//注册Nuget包System.Text.Encoding.CodePages中的编码到.NET Core
+
+            DateTime beforDT = System.DateTime.Now;
+
+            DirectoryInfo di = new DirectoryInfo(xlsPath);
+            var files = di.GetFiles("*.xlsx");
+
+            ThreadPool.SetMaxThreads(8, 8);
+
+            foreach (var file in files)
+            {
+                ThreadPool.QueueUserWorkItem(DoCovert, xlsPath + file.Name);
+            }
+
+            while (true)
+            {
+                Thread.Sleep(1000);//这句写着，主要是没必要循环那么多次。去掉也可以。
+                int maxWorkerThreads, workerThreads;
+                int portThreads;
+                ThreadPool.GetMaxThreads(out maxWorkerThreads, out portThreads);
+                ThreadPool.GetAvailableThreads(out workerThreads, out portThreads);
+                if (maxWorkerThreads - workerThreads == 0)
+                {
+                    break;
+                }
+            }
+
+            DateTime afterDT = System.DateTime.Now;
+            TimeSpan ts = afterDT.Subtract(beforDT);
+            Console.WriteLine("csv文件转换成功，耗时" + ts.Seconds.ToString() + "s\n");
+        }
+    }
+}
